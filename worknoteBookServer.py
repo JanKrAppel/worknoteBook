8ee3f58d36202965bddeede99c3dd92089db7473
Jan Kristoffer Appel <appel@physik.uni-kiel.de>
2015-09-09 15:45:01 +0200

    - Added tons of diagnostic output to console - Fixed bugs introduced and/or not fixed in earlier bug fixes

--- worknoteBookServer.py
+++ worknoteBookServer.py
@@ -10,6 +10,11 @@ import cherrypy
 class StaticDir(object):
     def __init__(self):
         pass
+        
+def print_enter(name):
+    print '-'*len(name)
+    print name
+    print '-'*len(name)
 
 class worknoteBookServer(object):
     
@@ -18,8 +23,10 @@ class worknoteBookServer(object):
         return abspath(expandvars(expanduser(dirname)))
         
     def __init__(self, config):
+        print_enter('__init__')
         self.config = config
         self.storagedir = self.__getabsdir(self.config[['server', 'storagedir']])
+        print 'Storagedir is "{:s}"'.format(self.storagedir)
         self.head = '''<!doctype html>
 <html>
     <head>
@@ -29,20 +36,29 @@ class worknoteBookServer(object):
     <body style="font-family: sans-serif">'''
         self.foot = '''    </body>
 </html>'''
+        print 'Loading chapters...'
         self.__load_chapters()
+        print 'Reloading worknotes...'
         self.reload_worknotes()
-        self.storagedir_locked = False
+        print 'Updating CherryPy config...'
         self.__update_config()
-        
+    
     def __update_config(self):
+        print_enter('__update_config')
+        print 'Server parameters:'
+        print '\turl: {:s}'.format(self.config[['server', 'url']])
+        print '\tport: {:d}'.format(self.config[['server', 'port']])
         cherrypy.config.update({'server.socket_host': self.config[['server', 'url']],
-                                'server.socket_port': self.config[['server', 'port']]})        
+                                'server.socket_port': self.config[['server', 'port']]})
+        print 'Mounting "{:s}" to /storage ...'.format(self.storagedir)
         cherrypy.tree.mount(StaticDir(), '/storage', config = {'/': {
                     'tools.staticdir.on': True,
                     'tools.staticdir.root': self.storagedir,
                     'tools.staticdir.dir': '.'
                 }})
         for chapter in self.chapters:
+            print 'Mounting "{:s}" to /{:s} ...'.format(self.chapters[chapter]['chapter_dir'],
+                                                         self.chapters[chapter]['link_name'])
             cherrypy.tree.mount(StaticDir(), '/{:s}'.format(self.chapters[chapter]['link_name']),
                                 config = {'/': {'tools.staticdir.on': True,
                                                 'tools.staticdir.root': self.chapters[chapter]['chapter_dir'],
@@ -50,11 +66,12 @@ class worknoteBookServer(object):
                                                 }})
                 
     def __load_chapters(self):
+        print_enter('__load_chapters')
         self.chapters = {}
         self.chapter_list = []
         chapters = self.config.get_sections()
         chapters.remove('server')
-        print chapters
+        print 'Chapters:', chapters
         for chapter in chapters:
             chapter_dir = self.__getabsdir(self.config[[chapter, 'chapter_dir']])
             link_name = chapter.replace(' ', '_')
@@ -67,16 +84,21 @@ class worknoteBookServer(object):
 
     @cherrypy.expose
     def reload_worknotes(self):
+        print_enter('reload_worknotes')
+        print 'Locking storage dir...'
         self.storagedir_locked = True
         self.worknotes = {}
         self.worknote_list = []
+        print 'Processing default storage directory...'
         self.__build_worknote_list(self.storagedir,
                                    self.worknote_list,
                                    self.worknotes)
         for chapter in self.chapters:
+            print 'Processing chapter "{:s}"...'.format(chapter)
             self.__build_worknote_list(self.chapters[chapter]['chapter_dir'],
                                        self.chapters[chapter]['worknote_list'],
                                        self.chapters[chapter]['worknotes'])
+        print 'Unlocking storage dir...'
         self.storagedir_locked = False
         head = self.head.format(metadata='<meta http-equiv="refresh" content="5; url=./">')
         foot = self.foot.format()
@@ -86,16 +108,22 @@ class worknoteBookServer(object):
         from worknote import Worknote
         from os.path import isdir, join, exists
         from os import listdir
+        print 'Processing directory "{:s}"...'.format(directory)
         for wn_workdir in [name for name in listdir(directory)
             if isdir(join(directory, name))
             and exists(join(join(directory, name), 'notedata.worknote'))]:
+                print 'Worknote:', wn_workdir
                 worknotes[wn_workdir] = Worknote(join(directory, wn_workdir))
                 worknote_list.append([wn_workdir, worknotes[wn_workdir].metadata.metadata['title'], worknotes[wn_workdir].metadata.metadata['date']])
+                print '\tTitle:', worknotes[wn_workdir].metadata.metadata['title']
+                print '\tBuilding HTML...'
                 worknotes[wn_workdir].build('HTML')
+                print '\tBuilding Beamer PDF...'
                 worknotes[wn_workdir].build('Beamer')
 
     @cherrypy.expose
     def index(self):
+        print_enter('index')
         head = self.head.format(metadata='<title>Workbook</title>\n')
         foot = self.foot.format()
         if self.storagedir_locked:
@@ -113,10 +141,13 @@ class worknoteBookServer(object):
     </main>
 {foot:s}'''
         wn_wrapper = '<li><a href="storage/{wn_dir:s}/Report.html">{wn_title:s}</a> ({wn_date:s}) <a href="{dl_link:s}">Download</a> <a href="storage/{wn_dir:s}/Beamer.pdf" target="_blank">Download PDF</a></li>\n'
+        print 'Building worknote list...'
         wn_list = ''
         index = 0
+        print 'Default storage dir...'
         for index, entry in enumerate(self.worknote_list):
-            wn_workdir, title, date = entry            
+            wn_workdir, title, date = entry           
+            print 'Worknote:', wn_workdir
             if '\\today' in date:
                 from datetime import datetime
                 now = datetime.now()
@@ -128,11 +159,13 @@ class worknoteBookServer(object):
                                          wn_date=date,
                                          dl_link='./download?index={index:d}'.format(index=index+1)) 
         for chapter in self.chapters:
+            print 'Chapter:', chapter
             wn_list += '<li><b>{:s}</b></br></li>\n'.format(chapter)
             wn_list += '<ol>\n'
             index += 1
             for subindex, entry in enumerate(self.chapters[chapter]['worknote_list']):
                 wn_workdir, title, date = entry            
+                print 'Worknote:', wn_workdir
                 if '\\today' in date:
                     from datetime import datetime
                     now = datetime.now()
@@ -148,7 +181,9 @@ class worknoteBookServer(object):
         
     @cherrypy.expose
     def download(self, index=None):
+        print_enter('download')
         if not index is None:
+            print 'Index:', index
             from worknoteBookHelpers import parse_index
             try:
                 index = parse_index(index)
@@ -160,6 +195,7 @@ class worknoteBookServer(object):
                     raise ValueError('Too many indices')
             except ValueError, e:
                 return str(e)
+            print 'Parsed index:', index
             if len(index) == 1:
                 index = index[0]
                 if index < 0 or index > len(self.worknote_list) - 1:
@@ -176,6 +212,7 @@ class worknoteBookServer(object):
                 return self.__serve_wn(self.chapters[chapter]['worknote_list'][index],
                                        self.chapters[chapter]['chapter_dir'])                
         else:
+            print 'No index, returning worknote list...'
             import json
             res = []
             for index, wn in enumerate(self.worknote_list):
@@ -187,53 +224,70 @@ class worknoteBookServer(object):
             return json.dumps(res)
             
     def __serve_wn(self, wn, storagedir):
+        print_enter('__serve_wn')
         from tempfile import gettempdir
         from os.path import join, exists
         from cherrypy.lib.static import serve_download
         from worknoteBookHelpers import zip_worknote
         wn_dir, wn_title, wn_date = wn
+        print 'Preparing to serve "{:s}" from "{:s}"...'.format(wn_dir, storagedir)
         tmpdir = gettempdir()
         fn_wnzip = '{wn_title:s}.zip'.format(wn_title=wn_title)
         dl_filepath = join(tmpdir, fn_wnzip)
+        print 'Zipping worknote...'
         zip_worknote(join(storagedir, wn_dir), dl_filepath)
         if exists(dl_filepath):
+            print 'Serving file "{:s}"...'.format(dl_filepath)
             return serve_download(dl_filepath, name=fn_wnzip)    
         else:
+            print 'ERROR: Zip file not created'
             return 'Error creating ZIP archive for download'
         
 
     @cherrypy.config(**{'response.timeout': 3600})
     @cherrypy.expose
     def upload(self, chapter=''):
+        print_enter('upload')
         from tempfile import gettempdir
         from shutil import copyfileobj, rmtree
         from os.path import join, split, exists
         from zipfile import ZipFile
         from worknoteBookHelpers import unzip_worknote
+        print 'Receiving file...'
         dst_file = join(gettempdir(), 'worknoteBook_upload.zip')   
         with open(dst_file, 'wb') as outfile:
             copyfileobj(cherrypy.request.body, outfile)
         if 'X-Worknote-Workdir' in cherrypy.request.headers:
             wn_dir = cherrypy.request.headers['X-Worknote-Workdir']
+            print 'Header set worknote directory: "{:s}"'.format(wn_dir)
         else:
             zipfile = ZipFile(dst_file, 'r')
             wn_dir = split(zipfile.namelist()[0])[0]
             zipfile.close()
-        if not chapter == '':
+            print 'File set worknote directory: "{:s}"'.format(wn_dir)
+        if chapter == '':
+            print 'Upload to default storage dir...'
             storagedir = self.storagedir
         else:
+            print 'Upload to chapter "{:s}"...'.format(chapter)
             if not chapter in self.chapters:
+                print 'ERROR: Chapter not found'
                 return 'Fail (Chapter {:s} not found)'.format(chapter)
             storagedir = self.chapters[chapter]['chapter_dir']
+            print '\tStorage dir: "{:s}"'.format(storagedir)
         if 'X-Worknote-Overwrite' in cherrypy.request.headers:
             overwrite = cherrypy.request.headers['X-Worknote-Overwrite'] == 'True'
+            print 'Overwrite set in header, emptying directory "{:s}"...'.format(join(storagedir, wn_dir))
             if  overwrite:
                 if exists(join(storagedir, wn_dir)):
                     rmtree(join(storagedir, wn_dir))
         try:
+            print 'Unzipping worknote...'
             unzip_worknote(dst_file, join(storagedir, wn_dir))
         except OSError, e:
+            print 'ERROR: Unzip failed'
             return 'Fail (' + str(e) + ')'
         else:
             self.reload_worknotes()
+            print 'Upload done'
             return 'Success'
